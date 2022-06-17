72c2e9e003af8022a3157acb95b7aed86558a8a4
PCG <PCG@DESKTOP-CQA7U9O>
2022-06-17 07:50:39 -0500

    Realizacion Evaluacion 03_01

--- src/main/java/com/mycompany/evaluacion_03_01/Evaluacion_03_01.java
+++ src/main/java/com/mycompany/evaluacion_03_01/Evaluacion_03_01.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.evaluacion_03_01;
 
+import Vista.JugadorVentana;
+
 /**
  *
  * @author PCG
@@ -12,6 +14,8 @@ package com.mycompany.evaluacion_03_01;
 public class Evaluacion_03_01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var v1 = new JugadorVentana();
+        
+        v1.setVisible(true); 
     }
 }
